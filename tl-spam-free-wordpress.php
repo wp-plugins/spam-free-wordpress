0716523e34af4e42e409a774245bdebdf638b875
toddlahman <toddlahman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-30 01:28:21 +0000

    Added global blocklist after code rewrite
    git-svn-id: https://plugins.svn.wordpress.org/spam-free-wordpress/tags/1.4.9@402621 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tl-spam-free-wordpress.php
+++ tl-spam-free-wordpress.php
@@ -3,10 +3,13 @@
 Plugin Name: Spam Free Wordpress
 Plugin URI: http://www.toddlahman.com/spam-free-wordpress/
 Description: Comment spam blocking plugin that uses anonymous password authentication to achieve 100% automated spam blocking with zero false positives, plus a few more features.
-Version: 1.4.8
+Version: 1.4.9
 Author: Todd Lahman, LLC
 Author URI: http://www.toddlahman.com/
 */
+
+// Plugin version
+$spam_free_wordpress_version = "1.4.9";
 
 /*
 	Copyright 2007 - 2011 by Todd Lahman, LLC.
@@ -29,8 +32,8 @@ Author URI: http://www.toddlahman.com/
 function add_default_data() {
 	$sfw_options = array(
 	'blocklist_keys' => '',
-	//'remote_blocked_list' => '',
-	'pw_field_size' => '20',
+	'remote_blocked_list' => '',
+	'pw_field_size' => '30',
 	'tab_index' => '',
 	'affiliate_msg' => '',
 	'toggle_stats_update' => 'disable',
@@ -47,32 +50,22 @@ register_activation_hook( __FILE__, 'add_default_data' );
 // The post comment passwords can be deleted also using the following SQL statement.
 // DELETE from wp_postmeta WHERE meta_key = "sfw_comment_form_password" ;
 
-/*
+
 // Runs remove_default_data function above when plugin deactivated
 // Uncomment this code block if you want the all options to be deleted when plugin is deactivated, this includes number of spam blocked
+/*
 function remove_default_data() {
 delete_option('spam_free_wordpress');
 delete_option('sfw_spam_hits');
+delete_option('remote_blocked_list');
 }
-
-// register_deactivation_hook( __FILE__, 'remove_default_data' );
 */
 
-// Plugin version
-$spam_free_wordpress_version = "1.4.8";
+// Uncomment this code block if you want the all options to be deleted when plugin is deactivated, this includes number of spam blocked
+//register_deactivation_hook( __FILE__, 'remove_default_data' );
 
 // variable used as global to retrieve option array for functions
 $wp_sfw_options = get_option('spam_free_wordpress');
-
-// Displays message on Admin settings page if using a Wordpress version older than 3.x
-function older_wp_notice() {
-	global $wp_version;
-
-	if ( version_compare($wp_version, '3.0', '<' ) ) {
-		echo '<table class="form-table"><tr><td><fieldset><label><hr /><h3>WARNING</h3><p>Your version of Wordpress is '.$wp_version.', which is too old to take advantage of the automated features built into Spam Free Wordpress. Please upgrade Wordpress.</p><hr /></label><br /></fieldset></td></tr></table>';
-	}
-}
-
 
 // Checks to see if comment form password exists and if not creates one in custom fields
 function sfw_comment_pass_exist_check() {
@@ -131,8 +124,7 @@ function wp_blocklist_check() {
 		// Skip empty lines
 		if ( empty($lkey) ) { continue; }
 
-		// Do some escaping magic so that '#' chars in the
-		// spam words don't break things:
+		// Can use '#' to comment out line in blocklist
 		$lkey = preg_quote($lkey, '#');
 
 		$pattern = "#$lkey#i";
@@ -143,18 +135,21 @@ function wp_blocklist_check() {
 	}
 	return false;
 }
-/*
+
 // Returns Remote Realtime Comment Blocklist
 function wp_realtime_blocklist_check() {
 	global $wp_sfw_options;
 	
 	// Gets IP address of commenter
 	$comment_author_ip = get_remote_ip_address();
-
-	$remote_blocklist_keys = trim( wp_remote_get($wp_sfw_options['remote_blocked_list']) );
-	if ( '' == $remote_blocklist_keys )
-		return false; // If blocklist keys are empty
-	$remote_key = explode("\n", $remote_blocklist_keys );
+	// Retrieves remote blocklist url from database
+	$rbl_url = $wp_sfw_options['remote_blocked_list'];
+	// Uses a URL to retrieve a list of IP address in an array
+	$get_remote_blocklist = wp_remote_get($rbl_url);
+	
+	if ( '' == $rbl_url )
+		return false; // If blocklist keys are empty or url is not in the database
+	$remote_key = explode("\n", $get_remote_blocklist['body'] ); // Turns blocklist array into string and lists each IP address on new line
 
 	foreach ( (array) $remote_key as $rkey ) {
 		$rkey = trim($rkey);
@@ -162,8 +157,7 @@ function wp_realtime_blocklist_check() {
 		// Skip empty lines
 		if ( empty($rkey) ) { continue; }
 
-		// Do some escaping magic so that '#' chars in the
-		// spam words don't break things:
+		// Can use '#' to comment out line in blocklist
 		$rkey = preg_quote($rkey, '#');
 
 		$pattern = "#$rkey#i";
@@ -174,7 +168,6 @@ function wp_realtime_blocklist_check() {
 	}
 	return false;
 }
-*/
 
 // Customizable Affiliate link
 function custom_affiliate_link() {
@@ -196,11 +189,6 @@ function tl_spam_free_wordpress_comments_form() {
 	global $wp_version;
 	
 	$sfw_comment_form_password_var = get_post_meta( $post->ID, 'sfw_comment_form_password', true );
-	// Checks to see if comment form password exists and if not creates one in custom fields
-	$new_sfw_comment_form_pwd = wp_generate_password(12, false);
-	if( empty($sfw_comment_form_password_var) || !$sfw_comment_form_password_var  && comments_open() ) {
-	update_post_meta($post->ID, 'sfw_comment_form_password', $new_sfw_comment_form_pwd);
-	}
 	
 	$sfw_pw_field_size = $wp_sfw_options['pw_field_size'];
 	$sfw_tab_index = $wp_sfw_options['tab_index'];
@@ -213,7 +201,7 @@ function tl_spam_free_wordpress_comments_form() {
 		echo "<input type='hidden' name='comment_ip' id='comment_ip' value='".get_remote_ip_address()."' />";
 		// Reader must enter this password manually on the comment form
 		echo "<p><label for='pwd_text'>* Copy this password:</label>
-		<input type='text' value='".$sfw_comment_form_password_var."' onclick='this.select()' size='20' /></p>";
+		<input type='text' value='".$sfw_comment_form_password_var."' onclick='this.select()' size='".$sfw_pw_field_size."' /></p>";
 		echo "<p><label for='passthis'>* Type or paste password here:</label>
 		<input type='text' name='passthis' id='passthis' value='".$comment_passthis."' size='".$sfw_pw_field_size."' tabindex='".$sfw_tab_index."' /></p>";
 		// Shows how many comment spam have been killed on the comment form
@@ -230,11 +218,6 @@ function tl_spam_free_wordpress_comments_post() {
 	global $post;
 	
 	$sfw_comment_script = get_post_meta( $post->ID, 'sfw_comment_form_password', true );
-	// Checks to see if comment form password exists and if not creates one in custom fields
-	$new_sfw_comment_script_pwd = wp_generate_password(12, false);
-	if( empty($sfw_comment_script) || !$sfw_comment_script  && comments_open() ) {
-	update_post_meta($post->ID, 'sfw_comment_form_password', $new_sfw_comment_script_pwd);
-	}
 	
 	// If the reader is logged in don't require password for wp-comments-post.php
 	if ( !is_user_logged_in() ) {
@@ -247,31 +230,11 @@ function tl_spam_free_wordpress_comments_post() {
 		if ($_POST['comment_ip'] == '' || $_POST['comment_ip'] == wp_blocklist_check() )
 			wp_die( __('Spam Blocked by Spam Free Wordpress (local blocklist)', spam_counter()) );
 		
-		//if ($_POST['comment_ip'] == '' || $_POST['comment_ip'] == wp_realtime_blocklist_check() )
-		//	wp_die( __('Spam Blocked by Spam Free Wordpress (remote blocklist)', spam_counter()) );
+		if ($_POST['comment_ip'] == '' || $_POST['comment_ip'] == wp_realtime_blocklist_check() )
+			wp_die( __('Spam Blocked by Spam Free Wordpress (remote blocklist)', spam_counter()) );
 
 	}
 }
-
-/*
-May add hashed password check with hidden form field using function:
-wp_hash_password( $password );
-
-example:
-
-$wp_hasher = new PasswordHash(8, TRUE);
-	
-$password_hashed = '$P$B55D6LjfHDkINU5wF.v2BuuzO0/XPk/';
-$plain_password = 'test';
-	
-if($wp_hasher->CheckPassword($plain_password, $password_hashed)) {
-   echo "YES, Matched";
-}
-else {
-   echo "No, Wrong Password";
-}
-
-*/
 
 // Counts number of comment spam hits and stores in options database table
 function spam_counter() {
@@ -287,8 +250,7 @@ function display_spam_hits() {
 
 // Register Admin Options Page
 function register_spam_free_wordpress_options_page() {
-	add_options_page('Spam Free Wordpress Configuration', 'Spam Free Wordpress', 'manage_options', __FILE__, 'spam_free_wordpress_options_page');
-
+	add_options_page('Spam Free Wordpress Configuration', 'Spam Free Wordpress', 'manage_options', 'spam-free-wordpress-admin-page', 'spam_free_wordpress_options_page');
 }
 
 // Admin Settings Options Page function
@@ -310,7 +272,7 @@ function spam_free_wordpress_options_page() {
 <?php
 
 	echo '<table class="form-table">';
-		if ($_POST['action'] == 'update') {
+		if ($_POST['options']) {
 		
 		$new_wp_sfw_options = $_POST['wp_sfw_options'];
 		update_option('spam_free_wordpress', $new_wp_sfw_options);
@@ -319,35 +281,40 @@ function spam_free_wordpress_options_page() {
 		$msg_status = 'Spam Free Wordpress settings saved.';
 		_e('<div id="message" class="updated fade"><p>' . $msg_status . '</p></div>');
 		}
-
-		older_wp_notice ();
 ?>
 
 <table class="form-table">
 	<tr>
 		<td valign="top">
 			<h3>How Much Comment Spam Has Been Blocked?</h3>
-					<p>Comment Spam Blocked: <b><?php echo number_format(get_option('sfw_spam_hits')); ?></b></p>
+					<p>Comment Spam Blocked: <font style="BACKGROUND-COLOR: #ffffff"><b><?php echo number_format(get_option('sfw_spam_hits')); ?></b></font></p>
 				
 			<h3>Local Comment Blocklist</h3>
-			<p>The Local Blocklist is a list of blocked IP addresses stored in the blog database. When a comment comes from an IP address matching the Blocklist it will be blocked, which means you will never see it as waiting for approval or marked as spam. Blocked commenters will be able to view your blog, but any comments they submit will be blocked, which means not saved to the database, and they will see the message &#8220;Spam Blocked.&#8221; Enter one IP address (for example 192.168.1.1) per line. Wildcards like 192.168.1.* will not work.</p>
+			<p>The Local Blocklist is a list of blocked IP addresses stored in the blog database. When a comment comes from an IP address matching the Blocklist it will be blocked, which means you will never see it as waiting for approval or marked as spam. Blocked commenters will be able to view your blog, but any comments they submit will be blocked, which means not saved to the database, and they will see the message &#8220;Spam Blocked.&#8221;</p>
+			<p>Enter one IP address (for example 192.168.1.1) per line. Wildcards like 192.168.1.* will not work.</p>
+			<p><code>#</code> can be used to comment out an IP address.</p>
 				<fieldset>
 					<label><textarea name="wp_sfw_options[blocklist_keys]" cols='40' rows='12' ><?php echo $wp_sfw_options['blocklist_keys']; ?></textarea></label><br />
 				</fieldset>
-				
 
+			<h3>Remote Comment Blocklist</h3>
+			<p>The Remote Comment Blocklist accesses a text file list of IP addresses on a remote server to block comment spam. This allows a global IP address blocklist to be shared with multiple blogs. It is also possible to use the Local Comment Blocklist for blog specific blocking, and the Remote Comment Blocklist for global blocking used by mutliple blogs at the same time. Remote Comment Blocklist works exactly the same way as the Local Comment Blocklist, except it is on a remote server. The URL to the remote text file could be for example: <code>http://www.example.com/mybl/bl.txt</code></p>
+			<p><code>#</code> can be used to comment out an IP address.</p>
+				<fieldset>
+					<label><p><input type="text" size="60" name="wp_sfw_options[remote_blocked_list]" value="<?php echo $wp_sfw_options['remote_blocked_list']; ?>" /> Enter URL to remote text file.</p></label>
+				</fieldset>
 				
 			<h3>Password Form Customization</h3>
 				<fieldset>
-					<label><p>Password Field Size <input type="text" name="wp_sfw_options[pw_field_size]" value="<?php echo $wp_sfw_options['pw_field_size']; ?>" /> Default is 20.</label>
+					<label><p><input type="text" name="wp_sfw_options[pw_field_size]" size="4" value="<?php echo $wp_sfw_options['pw_field_size']; ?>" /> Password Field Size. Default is 30.</label>
 				</fieldset>
 				<fieldset>
-					<label>Tab Index </font><input type="text" name="wp_sfw_options[tab_index]" value="<?php echo $wp_sfw_options['tab_index']; ?>" /></p></label>
+					<label><input type="text" name="wp_sfw_options[tab_index]" size="4" value="<?php echo $wp_sfw_options['tab_index']; ?>" /> Tab Index </p></label>
 				</fieldset>
 				
 			<h3>Comment Form Spam Stats</h3>
 			<p>When spam comment stats are ON they will be shown alongside a nofollow link to spamfreewordpress.com, and if an affiliate ID is entered below the link to spamfreewordpress.com will be transformed into the affiliate link.</p>
-				<label><p>Turn Spam Stats On or Off
+				<label><p>
 					<select name="wp_sfw_options[toggle_stats_update]" class="toggle_stats">
 						<option value="disable" <?php selected( 'disable', $wp_sfw_options['toggle_stats_update'] ); ?> ><?php _e( 'Spam Stats OFF' ) ?></option>
 						<option value="enable" <?php selected( 'enable', $wp_sfw_options['toggle_stats_update'] ); ?> ><?php _e( 'Spam Stats ON' ) ?></option>
@@ -356,13 +323,18 @@ function spam_free_wordpress_options_page() {
 				
 			<h3>Remove HTML from Comments</h3>
 			<p>It is very common for manual and automated comment spam to include a URL that links to a web site. This feature will automatically strip out HTML from comments so that links will show up as plain text, and it removes the allowed HTML tags from below the comment text box.</p>
-				<label><p>Turn HTML Filter On or Off
+				<label><p>
 					<select name="wp_sfw_options[toggle_html]" class="toggle_html">
 						<option value="disable" <?php selected( 'disable', $wp_sfw_options['toggle_html'] ); ?> ><?php _e( 'Strip HTML OFF' ) ?></option>
 						<option value="enable" <?php selected( 'enable', $wp_sfw_options['toggle_html'] ); ?> ><?php _e( 'Strip HTML ON' ) ?></option>
 					</select>
 				</p></label>
 				
+			<h3>Pingbacks and Trackbacks</h3>
+			<p>The plugin below will close pingbacks and trackbacks on all posts and pages on a blog.</p>
+			<p>Download the Auto Close Pings and Trackbacks plugin from the <a href="http://www.toddlahman.com/spam-free-wordpress/" target="_blank">Spam Free Wordpress</a> homepage.</p>
+			<p>To make sure pingbacks and trackbacks are closed on future posts and pages, go to <code>Settings -> Discussion</code> and uncheck the box next to <code>Allow link notifications from other blogs (pingbacks and trackbacks)</code>.</p>
+		
 			<h3>Share Link Custom Message</h3>
 			<p>Customize a URL link to the Spam Free Wordpress plugin page below if you want to share it with others somewhere else on your blog other than the comment form.</p>
 				<fieldset>
@@ -371,46 +343,35 @@ function spam_free_wordpress_options_page() {
 			<p>Copy and paste the line of code below into a template file to display the custom share link.</p>
 			<code>&lt;?php if(function_exists('custom_affiliate_link')) { custom_affiliate_link(); } ?&gt;</code>
 			
-			<input type="hidden" name="page_options" value="<?php echo $wp_sfw_options ?>" />
-			<input type="hidden" name="action" value="update" />
 			<p class="submit">
-			<input type="submit" name="Submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+			<input type="submit" name="options" class="button-primary" value="<?php _e('Save Changes') ?>" />
 			</p>
 </form>
+
+			<h3>Donate to Help Fight Spam</h3>
+			<p>Your donation will help WordPress become the world's first and only comment spam free blogging platform.</p>
+			<p>Even $1 can make a difference.</p>
+			<p>
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="SFVH6PCCC6TLG">
+			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+			</form>
+			</p>
+
+			<h3>Installation Instructions</h3>
+			<p>Complete installation instructions are available at <a href="http://www.toddlahman.com/spam-free-wordpress/" target="_blank">Spam Free Wordpress</a>.
+
+			<hr />
+			<h4><a href="http://www.toddlahman.com/" target="_blank">Spam Free Wordpress</a> was built and created by Todd Lahman.</h4>
 			
 		</td>
 			
 		<td valign="top">
 			<div id="sideblock" style="float:right;width:275px;margin-left:10px;"> 		 
-				<iframe width="275" height="800" frameborder="0" src="http://www.toddlahman.com/plugin-news/sfw/spw-plugin-news.html?utm_source=sfw-plugin&utm_medium=sfw-plugin&utm_campaign=sfw-plugin"></iframe>
+				<iframe width="275" height="1070" frameborder="0" src="http://www.toddlahman.com/plugin-news/sfw/spw-plugin-news.html?utm_source=sfw-plugin&utm_medium=sfw-plugin&utm_campaign=sfw-plugin"></iframe>
 			</div>
-		</td>
-	</tr>
-</table>
-
-<table class="form-table">
-	<tr>
-		<td>
-		<h3>Instructions</h3>
-			<p>The password comment form field is required for <strong>Spam Free Wordpress</strong> to block comment spam. Below are instructions to make sure the comment form password field is working properly.</p>
-			<p>If you are running Wordpress 3.x and up, and your comments.php file is using the comment_form() function to output the comment form fields, then <strong>Spam Free Wordpress</strong> will automatically add the password field to your comment form.</p>
-			<p>If you don't see a password field in your comment form when you visit a post page then follow the instructions below.</p>
-			<p>If you are running Wordpress 3.x and up, and your comments.php file IS NOT using the comment_form() function to output the comment form fields, then you will need to copy and paste the following line of code to your comments.php file just below the website address, or email address field.<br /><code>&lt;?php if(function_exists('tl_spam_free_wordpress_comments_form')) { tl_spam_free_wordpress_comments_form(); } ?&gt;</code></p>
-			<p>If you are using Wordpress 2.8 or 2.9 then you will need to copy and paste the following line of code to your comments.php file just below the website address, or email address field.<br /><code>&lt;?php if(function_exists('tl_spam_free_wordpress_comments_form')) { tl_spam_free_wordpress_comments_form(); } ?&gt;</code></p>
-			<p>Refer to the readme.txt file for more detailed information.</p>
-			<p><strong>Thesis Theme</strong></p>
-			<p>Go to <a href="http://ngurl.me/thesis" target="_blank">Thesis</a> -> Custom File Editor, choose custom_functions.php, then click Edit selected file. Add the following line of code to that file.</p>
-			<p><code>add_action('thesis_hook_comment_field', 'tl_spam_free_wordpress_comments_form');</code></p>
-			<p>Save changes.</p>
-		</td>
-	</tr>
-</table>
-
-<table class="form-table">
-	<tr>
-		<td>
-			<hr />
-			<h4><a href="http://www.toddlahman.com/" target="_blank">Spam Free Wordpress</a> was built and created by Todd Lahman.</h4>
 		</td>
 	</tr>
 </table>

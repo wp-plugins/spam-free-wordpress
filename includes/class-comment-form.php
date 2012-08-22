32f17cb69a8cd64b4bdb3b3a3fa4047e8a9e4a01
toddlahman <toddlahman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-22 03:01:28 +0000

    Add 1.7.8.4
    git-svn-id: https://plugins.svn.wordpress.org/spam-free-wordpress/tags/1.7.8.4@588681 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-comment-form.php
+++ includes/class-comment-form.php
@@ -141,11 +141,14 @@ class SFWform {
 
 		$req = get_option( 'require_name_email' );
 		$aria_req = ( $req ? " aria-required='true'" : '' );
+		if ( get_option( 'require_name_email' ) ) {
+			$req_field = 'required';
+		}
 		$fields =  array(
 			'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Name' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
-						'<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' required /></p>',
+						'<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' ' . $req_field . ' /></p>',
 			'email'  => '<p class="comment-form-email"><label for="email">' . __( 'Email' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
-						'<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' required /></p>',
+						'<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' ' . $req_field . ' /></p>',
 			'url'    => '<p class="comment-form-url"><label for="url">' . __( 'Website' ) . '</label>' .
 						'<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></p>',
 		);

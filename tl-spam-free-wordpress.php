d8000b06b9193990fa7cd8643a539b48ade2bc9e
toddlahman <toddlahman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-30 16:42:42 +0000

    cleanup
    git-svn-id: https://plugins.svn.wordpress.org/spam-free-wordpress/tags/1.8.3@592506 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tl-spam-free-wordpress.php
+++ tl-spam-free-wordpress.php
@@ -3,7 +3,7 @@
 Plugin Name: Spam Free Wordpress
 Plugin URI: http://www.toddlahman.com/spam-free-wordpress/
 Description: Comment spam blocking plugin that uses anonymous password authentication to achieve 100% automated spam blocking with zero false positives, plus a few more features.
-Version: 1.8.2
+Version: 1.8.3
 Author: Todd Lahman, LLC
 Author URI: http://www.toddlahman.com/
 License: GPLv3
@@ -17,7 +17,7 @@ License: GPLv3
 
 
 if ( !defined('SFW_VERSION') )
-	define( 'SFW_VERSION', '1.8.2' );
+	define( 'SFW_VERSION', '1.8.3' );
 if ( !defined('SFW_WP_REQUIRED') )
 	define( 'SFW_WP_REQUIRED', '3.1' );
 if (!defined('SFW_WP_REQUIRED_MSG'))
@@ -38,9 +38,12 @@ if(!defined( 'SFW_API_KEY_URL' ) )
 // Ready for translation
 load_plugin_textdomain( 'spam-free-wordpress', false, dirname( plugin_basename( __FILE__ ) ) . '/translations' );
 
+require_once( SFW_PATH . 'includes/class-key-sfw.php' );
+require_once( SFW_PATH . 'includes/functions.php' );
+require_once( SFW_PATH . 'includes/class-comment-form.php' );
+require_once( SFW_PATH . 'includes/legacy.php' );
 
 if ( SFW_IS_ADMIN ) {
-	require_once( SFW_PATH . 'admin/class-db-sfw.php' );
 	require_once( SFW_PATH . 'admin/class-menu-sfw.php' );
 	require_once( SFW_PATH . 'admin/class-tool-tips-sfw.php' );
 }
@@ -56,11 +59,11 @@ if ( get_option('sfw_version') && version_compare( get_option('sfw_version'), SF
 
 // Set the default settings if not already set
 if( !get_option( 'spam_free_wordpress' ) ) {
-	SFW_DB::sfw_default();
+	sfw_default();
 }
 
 // Runs add_default_data function above when plugin activated
-register_activation_hook( __FILE__, array('SFW_DB', 'sfw_default') );
+register_activation_hook( __FILE__, 'sfw_default' );
 
 /*
 * Since 1.8
@@ -70,14 +73,9 @@ if( get_option('spam_free_wordpress') ) {
 	$sfw_options = get_option('spam_free_wordpress');
 	
 	if ( !$sfw_options['bl_keys'] ) {
-		SFW_DB::upgrade_db();
+		sfw_upgrade_db();
 	}
 }
-
-require_once( SFW_PATH . 'includes/class-key-sfw.php' );
-require_once( SFW_PATH . 'includes/functions.php' );
-require_once( SFW_PATH . 'includes/class-comment-form.php' );
-require_once( SFW_PATH . 'includes/legacy.php' );
 
 //Pingbacks and trackbacks are closed automatically one time only
 if( get_option( 'sfw_close_pings_once' ) ) {
@@ -207,13 +205,149 @@ if( !isset( $sfw_lic_nag ) || get_option( 'sfw_lic_nag' ) < '2' ) {
 }
 
 
+/*******************************************
+* Database default and upgrade
+********************************************/
+// Add default database settings on plugin activation
+function sfw_default() {
+	if( !get_option( 'spam_free_wordpress' ) ) {
+		if ( version_compare( get_bloginfo( 'version' ), SFW_WP_REQUIRED, '<' ) ) {
+			deactivate_plugins( basename( __FILE__ ) );
+			wp_die( SFW_WP_REQUIRED_MSG );
+		} else {
+			$sfw_options = array(
+			'bl_keys' => '',
+			'cf_spam_stats' => 'off',
+			'cf_html' => 'on',
+			'website_url' => 'on',
+			'author_link' => 'on',
+			'ping_status' => 'on',
+			'comment_form' => 'on',
+			'cf_msg' => '',
+			'api_key' => ''
+			);
+			update_option( 'spam_free_wordpress', $sfw_options );
+
+			// Close pingback default settings
+			update_option( 'default_ping_status', 'closed' );
+			update_option( 'default_pingback_flag', '' );
+			}
+		
+		if( !get_option( 'sfw_spam_hits' ) ) {
+			update_option( 'sfw_spam_hits', '1' );
+		}
+	}
+}
+		
+
+// Since 1.8
+function sfw_upgrade_db() {
+	if ( version_compare( get_bloginfo( 'version' ), SFW_WP_REQUIRED, '<' ) ) {
+		deactivate_plugins( basename( __FILE__ ) );
+		wp_die( SFW_WP_REQUIRED_MSG );
+	} else {
+		$sfw_options = get_option('spam_free_wordpress');
+				
+		if( isset( $sfw_options['blocklist_keys'] ) ) {
+			$sfw_options['bl_keys'] = $sfw_options['blocklist_keys'];
+		} else {
+			$sfw_options['bl_keys'] = '';
+		}
+				
+		if( isset( $sfw_options['toggle_stats_update'] ) ) {
+			$sfw_options['cf_spam_stats'] = $sfw_options['toggle_stats_update'];
+				if( $sfw_options['cf_spam_stats'] == 'disable' ) {
+					$sfw_options['cf_spam_stats'] = 'off';
+				} elseif( $sfw_options['cf_spam_stats'] == 'enable' ) {
+					$sfw_options['cf_spam_stats'] = 'on';
+				}
+		} else {
+			$sfw_options['cf_spam_stats'] = 'off';
+		}
+				
+		if( isset( $sfw_options['toggle_html'] ) ) {
+			$sfw_options['cf_html'] = $sfw_options['toggle_html'];
+				if( $sfw_options['cf_html'] == 'disable' ) {
+					$sfw_options['cf_html'] = 'off';
+				} elseif( $sfw_options['cf_html'] == 'enable' ) {
+					$sfw_options['cf_html'] = 'on';
+				}
+		} else {
+			$sfw_options['cf_html'] = 'on';
+		}
+				
+		if( isset( $sfw_options['remove_author_url_field'] ) ) {
+			$sfw_options['website_url'] = $sfw_options['remove_author_url_field'];
+			if( $sfw_options['website_url'] == 'disable' ) {
+					$sfw_options['website_url'] = 'off';
+				} elseif( $sfw_options['website_url'] == 'enable' ) {
+					$sfw_options['website_url'] = 'on';
+				}
+		} else {
+			$sfw_options['website_url'] = 'on';
+		}
+				
+		if( isset( $sfw_options['remove_author_url'] ) ) {
+			$sfw_options['author_link'] = $sfw_options['remove_author_url'];
+			if( $sfw_options['author_link'] == 'disable' ) {
+					$sfw_options['author_link'] = 'off';
+				} elseif( $sfw_options['author_link'] == 'enable' ) {
+					$sfw_options['author_link'] = 'on';
+				}
+		} else {
+			$sfw_options['author_link'] = 'on';
+		}
+				
+		if( isset( $sfw_options['ping_status'] ) ) {
+			$sfw_options['ping_status'] = $sfw_options['ping_status'];
+				if( $sfw_options['ping_status'] == 'closed' ) {
+					$sfw_options['ping_status'] = 'off';
+				} elseif( $sfw_options['ping_status'] == 'open' ) {
+					$sfw_options['ping_status'] = 'on';
+				}
+		} else {
+			$sfw_options['ping_status'] = 'on';
+		}
+				
+		if( isset( $sfw_options['comment_form'] ) ) {
+			$sfw_options['comment_form'] = $sfw_options['comment_form'];
+		} else {
+			$sfw_options['comment_form'] = 'on';
+		}
+				
+		if( isset( $sfw_options['comment_form'] ) ) {
+			$sfw_options['comment_form'] = $sfw_options['comment_form'];
+		} else {
+			$sfw_options['comment_form'] = 'on';
+		}
+				
+		if( isset( $sfw_options['special_msg'] ) ) {
+			$sfw_options['cf_msg'] = $sfw_options['special_msg'];
+		} else {
+			$sfw_options['cf_msg'] = '';
+		}
+				
+		if( isset( $sfw_options['api_key'] ) ) {
+			$sfw_options['api_key'] = $sfw_options['api_key'];
+		} else {
+			$sfw_options['api_key'] = '';
+		}
+					
+		update_option( 'spam_free_wordpress', $sfw_options );
+	}
+}
+
+/*******************************************
+* Database default and upgrade END
+********************************************/
+
 // For testing only
 function sfw_delete() {
 	delete_option( 'spam_free_wordpress' );
 	delete_option( 'sfw_close_pings_once' );
 }
 
-register_deactivation_hook( __FILE__, 'sfw_delete' );
+//register_deactivation_hook( __FILE__, 'sfw_delete' );
 
 /*
 * For troubleshooting Unexpect Output errors

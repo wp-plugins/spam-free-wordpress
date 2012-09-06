fb2aa446369603914676dc3e54a23f2495d41824
toddlahman <toddlahman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-06 09:32:53 +0000

    Add 1.8.5
    git-svn-id: https://plugins.svn.wordpress.org/spam-free-wordpress/tags/1.8.5@595308 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/before-wp-3_3/sfw-ipwd-dev.js
+++ js/before-wp-3_3/sfw-ipwd-dev.js
@@ -1,4 +1,25 @@
 // For jQuery older than version 1.7, and WordPress prior to version 3.3
+/*
+(function($) {
+	$(document).ready(function() {
+		// get the comment post id
+		$('#comment_post_ID').each(function() {
+			$this = $(this);
+			pid = $this.val();
+		});		
+		// get password and remote IP address
+		$( '#comment' ).bind( 'keydown', function() {
+			$.post(sfw_ipwd.ajaxurl, { action: 'sfw_i_pwd', post_id : pid }, function( response ) {
+				$( '.pwddefault' ).val( response );
+			});
+			$.post(sfw_client_ip.ajaxurl, { action: 'sfw_cip' }, function( response ) {
+				$( '#comment_ip' ).val( response );
+			});
+		});
+	});
+})(jQuery);
+*/
+
 (function($) {
 	$(document).ready(function() {
 		// get the comment post id
@@ -6,22 +27,12 @@
 			$this = $(this);
 			pid = $this.val();
 		});
-		
-		//clear default value and add '.not-empty' class on click
-		$('#comment').bind( 'focus', function(){
-			$.post(sfw_ipwd.ajaxurl, { action: 'sfw_i_pwd', post_id : pid }, function( response ) {
-				$( '.pwddefault' ).val( response );
-				$( '#comment_ready' ).html('<strong>Please leave your comment now.</strong>');
-				return false;
-			});
-		});
-		
-		// get remote IP address
+		// get password and remote IP address
 		$( '#comment' ).bind( 'keydown', function() {
-			$.post(sfw_client_ip.ajaxurl, { action: 'sfw_cip' }, function( response ) {
-				$( '#comment_ip' ).val( response );
-				return false;
-			});
+			$.post(sfw_ipwd.ajaxurl, { action: 'sfw_i_pwd', post_id : pid }, function( response ) {
+				$( '.pwddefault' ).val( response.pwd );
+				$( '#comment_ip' ).val( response.ip );
+			}, 'json');
 		});
 	});
 })(jQuery);
